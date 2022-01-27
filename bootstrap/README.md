3f9a1cd0b5ebc90354f01a8a92dd3e937347ef0b
Clenio21 <89187752+Clenio21@users.noreply.github.com>
2022-01-27 10:32:36 +0100

    Add files via upload

---
+++ bootstrap/README.md
@@ -0,0 +1,2 @@
+# -SIGEE-SISTEMA-INTEGRADDO-DE-GEST-O-ESCOLAR-EKANDA
+sistema de gestão escolar EKANDA é capaz de automatizar as áreas financeira, pedagógica e acadêmica. Ou seja: o programa é capaz de realizar tarefas relacionadas às cobranças e à movimentação do caixa, além de também lidar com aspectos como cálculo de notas, emissão de boletins e outros documentos acadêmicos.
---
+++ bootstrap/js/manjolo.js
@@ -0,0 +1,16 @@
+function Voltar() {
+		window.history.go(-1);
+}
+
+function Imprimir() {
+		text=document.body.innerHTML;
+		print(text)
+}
+
+/* ========================================================================
+Welcome to Sistema de Gestão Escolar - EKANDA! Developed BY Eng.Clenio
+Welcome to Sistema de Gestão Escolar - EKANDA! Developed BY Eng.Clenio
+Welcome to Sistema de Gestão Escolar - EKANDA! Developed BY Eng.Clenio
+Welcome to Sistema de Gestão Escolar - EKANDA! Developed BY Eng.Clenio
+Welcome to Sistema de Gestão Escolar - EKANDA! Developed BY Eng.Clenio
+ * ======================================================================== */
